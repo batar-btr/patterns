0248f21a0d0d4c8ca9b0d8c1138ac175eba93f2e
batar-btr <btrn86@gmail.com>
2018-01-31 22:17:33 +0200

    init decorator

--- app/js/app.js
+++ app/js/app.js
@@ -1,51 +1,6 @@
 (() => {
 
-    var iterator = (function () {
-        var index = 0;
-        var data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        var length = data.length;
-
-        return {
-            next: function () {
-                var element;
-
-                if (!this.hasNext()) {
-                    return null;
-                }
-                element = data[index];
-                index++;
-                return element;
-
-            },
-            hasNext: function () {
-                return index < data.length;
-            },
-            reset: function(){
-                index = 0;
-            },
-            current: function() {
-                return data[index];
-            },
-            showState: function() {
-                console.log(`this index = ${index}`);
-                console.log(`this.length = ${length}`);
-            }
-            
-        }
-    })();
-
-    while (iterator.hasNext()) {
-        console.log(iterator.next()*10000);
-    }
-
-    
-    iterator.showState();
-    iterator.reset();
-    console.log(iterator.current());
-    iterator.next();
-    console.log(iterator.current());
-    iterator.next();
-    console.log(iterator.current());
+   
     
 })();
 
