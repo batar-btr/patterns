f390788ca79e8c059edbfac420eeef07fcaa1276
batar-btr <btrn86@gmail.com>
2018-01-31 12:55:34 +0200

    initial factory

--- app/dist/js/app.js
+++ app/dist/js/app.js
@@ -1,80 +1,55 @@
 'use strict';
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
 (function () {
 
-    function Moon() {
-        var instance = this;
-        if (_typeof(Moon.instance) === 'object') {
-            return Moon.instance;
-        }
+    function CarMaker() {};
 
-        this.name = 'planet';
-        this.color = 'gray';
-        this.getInstance = function () {
-            return instance;
-        };
-
-        Moon.instance = this;
-    }
-
-    var planet = new Moon();
-    var planet2 = new Moon();
-
-    console.log(planet == planet2);
-    console.log(planet.getInstance());
-
-    function Box(x, y) {
-        console.log('start constructor....');
-        this.x = x;
-        this.y = y;
-    }
-
-    // var obj = {
-    //     w: 100,
-    //     h: 200,
-    //     get box1() {
-
-    //         if(typeof Box.clouds === 'object' ) {
-    //             return Box.clouds;
-    //         }
-    //         Box.clouds = new Box(this.w, this.h);
-    //         return Box.clouds;
-    //     }
-    // }
-
-    // var boxA = obj.box1;
-    // var boxB = obj.box1;
-    // console.log(boxA===boxB);
-
-    var obj = {
-        w: 100,
-        h: 200,
-        get box1() {
-            //    this.box1.www = 10;
-            var obj = new Box(this.w, this.h);
-            delete this.box1;
-            return this.box1 = obj;
-        }
+    CarMaker.prototype.drive = function () {
+        console.log('Vroom, i have ' + this.wheels + ' wheels');
     };
 
-    var boxA = obj.box1;
-    var boxB = obj.box1;
-    var boxC = obj.box1;
-    var boxD = obj.box1;
-    var boxE = obj.box1;
-    console.log(boxA === boxD);
-    console.log(boxA === boxC);
-    console.log(boxA === boxB);
-    console.log(boxA === boxE); // true
-    // console.log(obj)
+    CarMaker.factory = function (type) {
+        var constr = type,
+            newcar;
 
-    // function www() {
-    //     www.bbb = 10;
-    //     console.log(www.bbb);
-    // };
+        if (typeof CarMaker[constr] !== 'function') {
+            throw {
+                name: 'Error',
+                message: constr + ' doesn\'t exist'
+            };
+        };
+        if (typeof CarMaker[constr].drive !== 'function') {
+            CarMaker[constr].prototype = new CarMaker();
+        }
 
-    // www();
+        // create new example
+
+        newcar = new CarMaker[constr]();
+        return newcar;
+    };
+
+    CarMaker.Compact = function () {
+        this.wheels = 4;
+    };
+
+    CarMaker.Large = function () {
+        this.wheels = 8;
+    };
+
+    CarMaker.Track = function () {
+        this.wheels = 18;
+    };
+
+    var car = CarMaker.factory('Compact');
+    var bus = CarMaker.factory('Large');
+    var track = CarMaker.factory('Track');
+
+    var park = [car, bus, track];
+
+    park.forEach(function (item) {
+        return item.drive();
+    });
+
+    // console.log(car);
 
 })();
