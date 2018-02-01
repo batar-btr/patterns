2fcc01524d346915b419d107931641901977dca8
batar-btr <btrn86@gmail.com>
2018-02-01 19:46:36 +0200

    decorator es5

--- app/dist/js/app.js
+++ app/dist/js/app.js
@@ -1,49 +1,45 @@
-"use strict";
+'use strict';
 
 (function () {
 
-    var iterator = function () {
-        var index = 0;
-        var data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        var length = data.length;
-
-        return {
-            next: function next() {
-                var element;
-
-                if (!this.hasNext()) {
-                    return null;
-                }
-                element = data[index];
-                index++;
-                return element;
-            },
-            hasNext: function hasNext() {
-                return index < data.length;
-            },
-            reset: function reset() {
-                index = 0;
-            },
-            current: function current() {
-                return data[index];
-            },
-            showState: function showState() {
-                console.log("this index = " + index);
-                console.log("this.length = " + length);
-            }
-
-        };
-    }();
-
-    while (iterator.hasNext()) {
-        console.log(iterator.next() * 10000);
+    function Sale(price) {
+        this.price = price || 100;
+        this.decorators_list = [];
     }
 
-    iterator.showState();
-    iterator.reset();
-    console.log(iterator.current());
-    iterator.next();
-    console.log(iterator.current());
-    iterator.next();
-    console.log(iterator.current());
+    Sale.decorators = {};
+
+    Sale.decorators.money = {
+        getPrice: function getPrice(price) {
+            return '$' + price + ' dollars';
+        }
+    };
+
+    Sale.decorators.grn = {
+        getPrice: function getPrice(price) {
+            return 'Ua' + price * 28 + ' grn';
+        }
+    };
+
+    Sale.prototype.decorate = function (decorator) {
+        this.decorators_list.push(decorator);
+    };
+
+    Sale.prototype.getPrice = function () {
+        var price = this.price,
+            i,
+            max = this.decorators_list.length,
+            name;
+
+        for (i = 0; i < max; i++) {
+            name = this.decorators_list[i];
+            price = Sale.decorators[name].getPrice(price);
+        }
+        return price;
+    };
+
+    var sale = new Sale(1000);
+    console.log(sale.getPrice());
+    sale.decorate('money');
+    console.log(sale.getPrice());
 })();
--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,48 @@
 (() => {
 
-   
-    
+    function Sale (price) {
+        this.price  = price || 100;
+        this.decorators_list = [];
+    }
+
+    Sale.decorators = {};
+
+    Sale.decorators.money = {
+        getPrice: function(price){
+            return `$${ price} dollars`
+        }
+    }
+
+    Sale.decorators.grn = {
+        getPrice: function(price){
+            return `Ua${ price * 28} grn`;
+        }
+    }
+
+    Sale.prototype.decorate = function(decorator) {
+        this.decorators_list.push(decorator)
+    }
+
+    Sale.prototype.getPrice = function () {
+        var price = this.price, 
+        i,
+        max = this.decorators_list.length,
+        name;
+
+        for(i = 0; i < max; i++) {
+            name = this.decorators[i];
+            price = Sale.decorators[name].getPrice(price);
+        }
+        return price;
+    }
+
+    var sale = new Sale(1000);
+    console.log(sale.getPrice());
+    sale.decorate('money');
+    console.log(sale.getPrice());
+
+
+
 })();
 
 
