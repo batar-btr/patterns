228e474ed7f08e8a397f33e7a4a94202ef81986e
batar-btr <btrn86@gmail.com>
2018-02-04 01:46:52 +0200

    observer

--- app/dist/js/app.js
+++ app/dist/js/app.js
@@ -1,86 +1,75 @@
 'use strict';
 
-(function () {
+var publisher = {
+    subscribers: {
+        any: []
+    },
+    subscribe: function subscribe(fn, type) {
+        type = type || 'any';
+        if (typeof this.subscribers[type] === 'undefined') {
+            this.subscribers[type] = [];
+        }
+        console.log(this.subscribers[type]);
+        this.subscribers[type].push(fn);
+    },
+    unsubscribe: function unsubscribe(fn, type) {
+        this.visitSubscribers('unsubscribe', fn, type);
+    },
+    publish: function publish(publication, type) {
+        this.visitSubscribers('publish', publication, type);
+    },
+    visitSubscribers: function visitSubscribers(action, arg, type) {
+        var pubtype = type || 'any',
+            subscribers = this.subscribers[pubtype],
+            i,
+            max = subscribers.length;
 
-    // player constructor
-    function Player(name) {
-        this.points = 0;
-        this.name = name;
-    }
-    // play method
-    Player.prototype.play = function () {
-        this.points += 1;
-        mediator.played();
-    };
-
-    // the scoreboard object
-    var scoreboard = {
-
-        // HTML element to be updated
-        element: document.getElementById('results'),
-
-        // update the score display
-        update: function update(score) {
-
-            var i,
-                msg = '';
-            for (i in score) {
-                if (score.hasOwnProperty(i)) {
-                    msg += '<p><strong>' + i + '<\/strong>: ';
-                    msg += score[i];
-                    msg += '<\/p>';
+        for (i = 0; i < max; i++) {
+            if (action === 'publish') {
+                subscribers[i](arg);
+            } else {
+                if (subscribers[i] === arg) {
+                    subscribers.splice(i, 1);
                 }
             }
-            this.element.innerHTML = msg;
         }
-    };
+    }
+};
 
-    var mediator = {
-
-        // all the players
-        players: {},
-
-        // 
-        setup: function setup() {
-            var players = this.players;
-            players.home = new Player('Home');
-            players.guest = new Player('Guest');
-        },
-
-        // someone plays, update the score
-        played: function played() {
-            var players = this.players,
-                score = {
-                Home: players.home.points,
-                Guest: players.guest.points
-            };
-
-            scoreboard.update(score);
-        },
-
-        // handle user interactions
-        keypress: function keypress(e) {
-            e = e || window.event; // IE
-            if (e.which === 49) {
-                // key "1"
-                mediator.players.home.play();
-                return;
-            }
-            if (e.which === 48) {
-                // key "0"
-                mediator.players.guest.play();
-                return;
-            }
+function makePublisher(object) {
+    var i;
+    for (i in publisher) {
+        if (publisher.hasOwnProperty(i) && typeof publisher[i] === 'function') {
+            object[i] = publisher[i];
         }
-    };
+        object.subscribers = { any: [] };
+    }
+}
 
-    // go!
-    mediator.setup();
-    window.onkeypress = mediator.keypress;
+var paper = {
+    daily: function daily() {
+        this.publish('bignews today');
+    },
+    monthly: function monthly() {
+        this.publish('monthly newspaper', 'monthly');
+    }
+};
 
-    // game over in 30 seconds
-    setTimeout(function () {
-        window.onkeypress = null;
-        alert('Game over!');
-    }, 30000);
-})();
+makePublisher(paper);
+console.dir(paper);
+
+var joe = {
+    drinkCoffe: function drinkCoffe(paper) {
+        console.log('Just read ' + paper);
+    },
+    sundayPreNap: function sundayPreNap(monthly) {
+        console.log('Just read ' + monthly);
+    }
+};
+
+paper.subscribe(joe.drinkCoffe);
+paper.subscribe(joe.sundayPreNap, 'monthly');
+
+paper.daily();
+paper.daily();
+paper.monthly();
--- app/js/app.js
+++ app/js/app.js
@@ -1,85 +1,73 @@
-(() => {
+var publisher = {
+    subscribers: {
+        any: []
+    },
+    subscribe: function (fn, type) {
+        type = type || 'any';
+        if (typeof this.subscribers[type] === 'undefined') {
+            this.subscribers[type] = [];
+        }
+        console.log(this.subscribers[type]);
+        this.subscribers[type].push(fn);
+    },
+    unsubscribe: function (fn, type) {
+        this.visitSubscribers('unsubscribe', fn, type);
+    },
+    publish: function (publication, type) {
+        this.visitSubscribers('publish', publication, type);
+    },
+    visitSubscribers: function (action, arg, type) {
+        var pubtype = type || 'any',
+            subscribers = this.subscribers[pubtype],
+            i,
+            max = subscribers.length;
 
-    // player constructor
-    function Player(name) {
-        this.points = 0;
-        this.name = name;
-    }
-    // play method
-    Player.prototype.play = function () {
-        this.points += 1;
-        mediator.played();
-    };
-
-    // the scoreboard object
-    var scoreboard = {
-
-        // HTML element to be updated
-        element: document.getElementById('results'),
-
-        // update the score display
-        update: function (score) {
-
-            var i, msg = '';
-            for (i in score) {
-                if (score.hasOwnProperty(i)) {
-                    msg += '<p><strong>' + i + '<\/strong>: ';
-                    msg += score[i];
-                    msg += '<\/p>';
+        for (i = 0; i < max; i++) {
+            if (action === 'publish') {
+                subscribers[i](arg);
+            } else {
+                if (subscribers[i] === arg) {
+                    subscribers.splice(i, 1);
                 }
             }
-            this.element.innerHTML = msg;
         }
-    };
+    }
+};
 
-
-    var mediator = {
-
-        // all the players
-        players: {},
-
-        // 
-        setup: function () {
-            var players = this.players;
-            players.home = new Player('Home');
-            players.guest = new Player('Guest');
-
-        },
-
-        // someone plays, update the score
-        played: function () {
-            var players = this.players,
-                score = {
-                    Home: players.home.points,
-                    Guest: players.guest.points
-                };
-
-            scoreboard.update(score);
-        },
-
-        // handle user interactions
-        keypress: function (e) {
-            e = e || window.event; // IE
-            if (e.which === 49) { // key "1"
-                mediator.players.home.play();
-                return;
-            }
-            if (e.which === 48) { // key "0"
-                mediator.players.guest.play();
-                return;
-            }
+function makePublisher(object) {
+    var i;
+    for (i in publisher) {
+        if (publisher.hasOwnProperty(i) && typeof publisher[i] === 'function') {
+            object[i] = publisher[i];
         }
-    };
+        object.subscribers = { any: [] };
+    }
+}
 
+var paper = {
+    daily: function () {
+        this.publish('bignews today');
+    },
+    monthly: function () {
+        this.publish('monthly newspaper', 'monthly');
+    }
+};
 
-    // go!
-    mediator.setup();
-    window.onkeypress = mediator.keypress;
+makePublisher(paper);
+console.dir(paper);
 
-    // game over in 30 seconds
-    setTimeout(function () {
-        window.onkeypress = null;
-        alert('Game over!');
-    }, 30000);
+var joe = {
+    drinkCoffe: function(paper){
+        console.log('Just read ' + paper);
+    },
+    sundayPreNap: function (monthly) {
+        console.log('Just read '+ monthly);
+    }
+};
 
-})();
+paper.subscribe(joe.drinkCoffe);
+paper.subscribe(joe.sundayPreNap, 'monthly');
+
+paper.daily();
+paper.daily();
+paper.monthly();
