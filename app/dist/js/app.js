f242e660763ff8b9900cfcdb2754a084889d9692
batar-btr <btrn86@gmail.com>
2018-02-03 16:49:53 +0200

    init mediator

--- app/dist/js/app.js
+++ app/dist/js/app.js
@@ -1,19 +1,86 @@
-"use strict";
+'use strict';
 
 (function () {
 
-    function fib(x) {
-        if (!fib.cache[x]) {
-            x > 1 ? fib.cache[x] = fib(x - 1) + fib(x - 2) : fib.cache[x] = x;
+    // player constructor
+    function Player(name) {
+        this.points = 0;
+        this.name = name;
+    }
+    // play method
+    Player.prototype.play = function () {
+        this.points += 1;
+        mediator.played();
+    };
+
+    // the scoreboard object
+    var scoreboard = {
+
+        // HTML element to be updated
+        element: document.getElementById('results'),
+
+        // update the score display
+        update: function update(score) {
+
+            var i,
+                msg = '';
+            for (i in score) {
+                if (score.hasOwnProperty(i)) {
+                    msg += '<p><strong>' + i + '<\/strong>: ';
+                    msg += score[i];
+                    msg += '<\/p>';
+                }
+            }
+            this.element.innerHTML = msg;
         }
-        return fib.cache[x];
-    }
+    };
 
-    fib.cache = {};
+    var mediator = {
 
-    console.log(fib(500));
+        // all the players
+        players: {},
 
-    for (var i = 0; i < 50; i++) {
-        console.log(fib(i));
-    }
+        // 
+        setup: function setup() {
+            var players = this.players;
+            players.home = new Player('Home');
+            players.guest = new Player('Guest');
+        },
+
+        // someone plays, update the score
+        played: function played() {
+            var players = this.players,
+                score = {
+                Home: players.home.points,
+                Guest: players.guest.points
+            };
+
+            scoreboard.update(score);
+        },
+
+        // handle user interactions
+        keypress: function keypress(e) {
+            e = e || window.event; // IE
+            if (e.which === 49) {
+                // key "1"
+                mediator.players.home.play();
+                return;
+            }
+            if (e.which === 48) {
+                // key "0"
+                mediator.players.guest.play();
+                return;
+            }
+        }
+    };
+
+    // go!
+    mediator.setup();
+    window.onkeypress = mediator.keypress;
+
+    // game over in 30 seconds
+    setTimeout(function () {
+        window.onkeypress = null;
+        alert('Game over!');
+    }, 30000);
 })();
--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,85 @@
 (() => {
-    
-    function fib(x) {
-        if(!fib.cache[x]) {
-            x > 1 ? fib.cache[x] = fib(x-1) + fib(x-2) : fib.cache[x] = x;
+
+    // player constructor
+    function Player(name) {
+        this.points = 0;
+        this.name = name;
+    }
+    // play method
+    Player.prototype.play = function () {
+        this.points += 1;
+        mediator.played();
+    };
+
+    // the scoreboard object
+    var scoreboard = {
+
+        // HTML element to be updated
+        element: document.getElementById('results'),
+
+        // update the score display
+        update: function (score) {
+
+            var i, msg = '';
+            for (i in score) {
+                if (score.hasOwnProperty(i)) {
+                    msg += '<p><strong>' + i + '<\/strong>: ';
+                    msg += score[i];
+                    msg += '<\/p>';
+                }
+            }
+            this.element.innerHTML = msg;
         }
-        return fib.cache[x];
-    }
+    };
 
-    fib.cache = {};
 
-    console.log(fib(500));
+    var mediator = {
 
-    for(var i = 0; i < 50; i++) {
-        console.log(fib(i));
-    }
+        // all the players
+        players: {},
+
+        // 
+        setup: function () {
+            var players = this.players;
+            players.home = new Player('Home');
+            players.guest = new Player('Guest');
+
+        },
+
+        // someone plays, update the score
+        played: function () {
+            var players = this.players,
+                score = {
+                    Home: players.home.points,
+                    Guest: players.guest.points
+                };
+
+            scoreboard.update(score);
+        },
+
+        // handle user interactions
+        keypress: function (e) {
+            e = e || window.event; // IE
+            if (e.which === 49) { // key "1"
+                mediator.players.home.play();
+                return;
+            }
+            if (e.which === 48) { // key "0"
+                mediator.players.guest.play();
+                return;
+            }
+        }
+    };
+
+
+    // go!
+    mediator.setup();
+    window.onkeypress = mediator.keypress;
+
+    // game over in 30 seconds
+    setTimeout(function () {
+        window.onkeypress = null;
+        alert('Game over!');
+    }, 30000);
 
 })();
